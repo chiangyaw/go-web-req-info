2fd0864403b93e761faf8f46d574474d0707f684
Felix Cheng <fefefe@gmail.com>
2021-06-08 14:43:24 +0000

    add support for X-Fowarded-Host

--- src/web-req-info.go
+++ src/web-req-info.go
@@ -14,9 +14,15 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	var cookies string = r.Header.Get("Cookie")
 	var requester_ip string = r.RemoteAddr
 	var xff string = r.Header.Get("X-Forwarded-For")
+	var xfh string = r.Header.Get("X-Forwarded-Host")
 	var query string = r.URL.RawQuery
 
 	fmt.Fprintf(w, "You requested to:\n%s\n\n", r.Method)
+
+	if len(xfh) > 0 {
+		fmt.Fprintf(w, "Your original request is for the host:\n%s\n\n", xfh)
+	}
+
 	fmt.Fprintf(w, "Your requested URL is:\n%s%s\n\n", r.Host, r.URL.Path)
 
 	if len(query) > 0 {
@@ -24,6 +30,7 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintf(w, "You request is from this IP address:\n%s\n\n", requester_ip)
+
 	if len(xff) > 0 {
 		fmt.Fprintf(w, "Your request is going through the following IP addresses:\n")
 		ips := strings.Split(xff, ", ")
@@ -32,12 +39,15 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, "\n")
 	}
+
 	if len(cookies) > 0 {
 		fmt.Fprintf(w, "Your Cookies are:\n%s\n\n", cookies)
 	}
+
 	if len(user_agent) > 0 {
 		fmt.Fprintf(w, "Your User-Agent is:\n%s\n", user_agent)
 	}
+
 	log.Printf("Received request from %s for path: %s from %s", requester_ip, r.URL.Path, user_agent)
 }
 
