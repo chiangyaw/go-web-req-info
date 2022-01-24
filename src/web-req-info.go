9d3fbb266da5a09bd535fdbd39ce073c1677b48d
Felix Cheng <fefefe@gmail.com>
2022-01-24 18:51:19 +0800

    put logics into functions

--- src/web-req-info.go
+++ src/web-req-info.go
@@ -17,89 +17,26 @@ var visit_count int = 0
 // Server to return info seen in the HTTP request
 func WebInfoServer(w http.ResponseWriter, r *http.Request) {
 
-	var user_agent string = r.Header.Get("User-Agent")
-	var cookies string = r.Header.Get("Cookie")
-	var requester_ip_port string = r.RemoteAddr
-	var xff string = r.Header.Get("X-Forwarded-For")
-	var xfh string = r.Header.Get("X-Forwarded-Host")
-	var query string = r.URL.RawQuery
-	var query_exists bool = false
 	var show_req_header bool = false
 
 	// count number of visitors since last restart
 	visit_count += 1
 
 	// show basic info in log
-	log.Printf("Received request from %s for path: %s from %s", requester_ip_port, r.URL.Path, user_agent)
+	log.Printf("Received request from %s for path: %s from %s", r.RemoteAddr, r.URL.Path, r.Header.Get("User-Agent"))
 
-	// show IP address of the client only if the requested URL is "/"
 	if r.URL.Path == "/" {
-		if len(xff) > 0 {
-			ips := strings.Split(xff, ", ")
-			fmt.Fprintf(w, "%s", ips[0])
-		} else {
-			ip_port_slice := strings.Split(requester_ip_port, ":")
-			fmt.Fprintf(w, "%s", strings.TrimSuffix(requester_ip_port, ":"+ip_port_slice[len(ip_port_slice)-1]))
-		}
-		// show more info about the request if the requested URL is "/info"
+		// show IP address of the client only if the requested URL is "/"
+		print_sender_ip(w, r)
 	} else if r.URL.Path == "/info" {
-
-		// show visitor count since last restart
-		fmt.Fprintf(w, "You are visitor number %d", visit_count)
-		fmt.Fprintf(w, " since last restart\n\n")
-
-		// show HTTP method
-		fmt.Fprintf(w, "You requested to:\n%s\n\n", r.Method)
-
-		// show HOST info
-		if len(xfh) > 0 {
-			fmt.Fprintf(w, "Your original request is for the host:\n%s\n\n", xfh)
-		}
-
-		// show URL requested
-		fmt.Fprintf(w, "Your requested URL is:\n%s%s\n\n", r.Host, r.URL.Path)
-
-		// show query
-		if query_exists {
-			fmt.Fprintf(w, "Query in your request:\n%s\n\n", query)
-		}
-
-		// show source IP address of IP packet
-		fmt.Fprintf(w, "Your request is from this IP address:\n%s\n\n", requester_ip_port)
-
-		// show IP addresses of the client and intermediate nodes
-		if len(xff) > 0 {
-			ips := strings.Split(xff, ", ")
-			for i, ip := range ips {
-				if i == 0 {
-					fmt.Fprintf(w, "Your original IP address is:\n")
-					fmt.Fprintf(w, "%s\n", ip)
-				} else if i == 1 {
-					fmt.Fprintf(w, "Your request is going through the following IP addresses:\n")
-					fmt.Fprintf(w, "%s\n", ip)
-				} else {
-					fmt.Fprintf(w, "%s\n", ip)
-				}
-			}
-			fmt.Fprintf(w, "\n")
-		}
-
-		// show cookie info
-		if len(cookies) > 0 {
-			fmt.Fprintf(w, "Your Cookies are:\n%s\n\n", cookies)
-		}
-
-		// show User-Agent info
-		if len(user_agent) > 0 {
-			fmt.Fprintf(w, "Your User-Agent is:\n%s\n\n", user_agent)
-		}
+		// show more info about the request if the requested URL is "/info"
+		print_req_info(w, r)
 	} else {
 		fmt.Fprintf(w, "Invalid URL")
 	}
 
 	// check whether query parameters exist
-	if len(query) > 0 {
-		query_exists = true
+	if len(r.URL.RawQuery) > 0 {
 
 		// req_hdr exists - return full HTTP request header
 		show_req_header, _ = strconv.ParseBool(r.URL.Query().Get("req_hdr"))
@@ -131,6 +68,88 @@ func WebInfoServer(w http.ResponseWriter, r *http.Request) {
 		for key, value := range r.Header {
 			for _, element := range value {
 				fmt.Fprintf(w, "%s: %s\n", key, element)
+			}
+		}
+		fmt.Fprintf(w, "\n")
+	}
+}
+
+// print IP address of HTTP request sender
+func print_sender_ip(w http.ResponseWriter, r *http.Request) {
+
+	var xff string = r.Header.Get("X-Forwarded-For")
+	var requester_ip_port string = r.RemoteAddr
+
+	if len(xff) > 0 {
+		ips := strings.Split(xff, ", ")
+		fmt.Fprintf(w, "%s", ips[0])
+	} else {
+		ip_port_slice := strings.Split(requester_ip_port, ":")
+		fmt.Fprintf(w, "%s", strings.TrimSuffix(requester_ip_port, ":"+ip_port_slice[len(ip_port_slice)-1]))
+	}
+}
+
+// print more info about the request if the requested URL is "/info"
+func print_req_info(w http.ResponseWriter, r *http.Request) {
+
+	var user_agent string = r.Header.Get("User-Agent")
+	var cookies string = r.Header.Get("Cookie")
+	var requester_ip_port string = r.RemoteAddr
+	var xff string = r.Header.Get("X-Forwarded-For")
+	var xfh string = r.Header.Get("X-Forwarded-Host")
+	var query string = r.URL.RawQuery
+
+	// show visitor count since last restart
+	fmt.Fprintf(w, "You are visitor number %d", visit_count)
+	fmt.Fprintf(w, " since last restart\n\n")
+
+	// show HTTP method
+	fmt.Fprintf(w, "You requested to:\n%s\n\n", r.Method)
+
+	// show HOST info
+	if len(xfh) > 0 {
+		fmt.Fprintf(w, "Your original request is for the host:\n%s\n\n", xfh)
+	}
+
+	// show URL requested
+	fmt.Fprintf(w, "Your requested URL is:\n%s%s\n\n", r.Host, r.URL.Path)
+
+	// show query
+	if len(r.URL.RawQuery) > 0 {
+		fmt.Fprintf(w, "Query in your request:\n%s\n\n", query)
+	}
+
+	// show source IP address of IP packet
+	fmt.Fprintf(w, "Your request is from this IP address:\n%s\n\n", requester_ip_port)
+
+	// show IP addresses of the client and intermediate nodes
+	print_xff_ips(w, xff)
+
+	// show cookie info
+	if len(cookies) > 0 {
+		fmt.Fprintf(w, "Your Cookies are:\n%s\n\n", cookies)
+	}
+
+	// show User-Agent info
+	if len(user_agent) > 0 {
+		fmt.Fprintf(w, "Your User-Agent is:\n%s\n\n", user_agent)
+	}
+}
+
+// print IP addresses from XFF header
+func print_xff_ips(w http.ResponseWriter, xff string) {
+
+	if len(xff) > 0 {
+		ips := strings.Split(xff, ", ")
+		for i, ip := range ips {
+			if i == 0 {
+				fmt.Fprintf(w, "Your original IP address is:\n")
+				fmt.Fprintf(w, "%s\n", ip)
+			} else if i == 1 {
+				fmt.Fprintf(w, "Your request is going through the following IP addresses:\n")
+				fmt.Fprintf(w, "%s\n", ip)
+			} else {
+				fmt.Fprintf(w, "%s\n", ip)
 			}
 		}
 		fmt.Fprintf(w, "\n")
